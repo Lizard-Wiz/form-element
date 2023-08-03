9f0dcfb0e0219f1d9dd1de6243e017a746ae4a55
Lizard Wiz <tommy.ackerman@yahoo.co.uk>
2023-08-03 17:07:38 +0100

    latest changes 3-8-23

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* INTERACTIVITY */
+/* Adds functionality! */
+
+const emailInput = document.querySelector("input#email");
+const form = document.querySelector("form");
+
+form.addEventListener('submit', function (event) {
+
+
+    
+}
